be6d6867a81cccf83a2ca6dcf1cd6bc8d36b064c
KIMURA TOMIOKO <122150562+KimuraTomioko@users.noreply.github.com>
2025-03-30 10:15:29 +0300

    initial commit

--- MarbellCamperHouses/urls.py
+++ MarbellCamperHouses/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('camper/', include('marbell.urls')),
+    path('', include('marbell.urls')),
 ]
 
 if settings.DEBUG:
--- marbell/urls.py
+++ marbell/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('main/', simple_index, name='main_page'),
-    path('main/es/', simple_index_spain, name='main_page_es')
+    path('', simple_index, name='main_page'),
+    path('es/', simple_index_spain, name='main_page_es')
 ]
